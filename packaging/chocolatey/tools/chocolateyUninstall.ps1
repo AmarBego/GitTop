11f0fba90eea78275b3bd1c5940e52209fd65ee1
Amar <begovicamar@proton.me>
2025-12-26 08:31:31 +0100

    feat release

--- packaging/chocolatey/tools/chocolateyUninstall.ps1
+++ packaging/chocolatey/tools/chocolateyUninstall.ps1
@@ -2,7 +2,6 @@ $ErrorActionPreference = 'Stop'
 
 $packageName = 'gittop'
 
-# Find the Inno Setup uninstaller in LocalAppData
 $uninstallPath = Join-Path $env:LOCALAPPDATA 'GitTop\unins000.exe'
 
 if (Test-Path $uninstallPath) {
@@ -19,6 +18,3 @@ if (Test-Path $uninstallPath) {
 else {
     Write-Warning "Uninstaller not found at $uninstallPath - GitTop may have been removed manually"
 }
-
-# Note: User settings are stored in $env:APPDATA\GitTop
-# These are preserved by default. To remove settings, delete that folder manually.
